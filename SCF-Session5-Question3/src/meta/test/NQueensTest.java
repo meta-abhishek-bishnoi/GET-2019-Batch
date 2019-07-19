bea349cc666f259ddc31ac3feded7b662481c449
ABHISHEK BISHNOI <abhishek.bishnoi@metacube.com>
2019-07-19 14:01:30 +0530

    Assignment Submission

--- SCF-Session5-Question3/src/meta/test/NQueensTest.java
+++ SCF-Session5-Question3/src/meta/test/NQueensTest.java
@@ -1,57 +1,60 @@
+/*
+ * This is test case class for N-Queens Problem 
+ * @author ABHISHEK BISHNOI
+ * @version 1.0
+ * @since 18-July-2019
+ */
 package meta.test;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
 import meta.utility.NQueens;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class NQueensTest {
 
-	@Test
-	public void testGetNQueen() {
-		int row =0;
-		int dimention = 4;
-		int data[][] = {
-				{0,0,0,0},
-				{0,0,0,0},
-				{0,0,0,0},
-				{0,0,0,0}
-		};
-		int expected[][] = {
-				{0,1,0,0},
-				{0,0,0,1},
-				{1,0,0,0},
-				{0,0,1,0}
-		};
-		NQueens object = new NQueens(data,row,dimention);
-		int actual[][]= object.getNQueen();
-		assertArrayEquals(expected,actual);
+	/*
+	 * Testing function with @Parameters 
+	 * So these test cases will executes one by one(By Iteration)
+	 * so here we don't require to write too much test cases
+	 * here in this array we are sending in order of:
+	 * row, dimension, empty board and result board
+	 */
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object[][]{
+				{ 0, 4, BoardData.arrayEmptyBoard1, BoardData.arrayResultBoard1 },
+				{ 0, 8, BoardData.arrayEmptyBoard2, BoardData.arrayResultBoard2 },
+				{ 0, 5, BoardData.arrayEmptyBoard3, BoardData.arrayResultBoard3}
+		});
+	}
+	// 
+	private int row;
+	private int dimension;
+	private int board[][];
+	private int expected[][];
+	/*
+	 * 
+	 */
+	public NQueensTest(int row, int dimension, int board[][], int expected[][]){
+		this.row = row;
+		this.dimension = dimension;
+		this.board = board;
+		this.expected = expected;
 	}
 	@Test
-	public void testGetNQueen1() {
-		int row =0;
-		int dimention = 8;
-		int data[][] = {
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0}
-		};
-		int expected[][] = {
-				{1,0,0,0,0,0,0,0},
-				{0,0,1,0,0,0,0,0},
-				{0,0,0,0,0,1,0,0},
-				{0,0,0,0,0,0,0,1},
-				{0,0,0,0,0,0,1,0},
-				{0,0,0,1,0,0,0,0},
-				{0,1,0,0,0,0,0,0},
-				{0,0,0,0,1,0,0,0}
-		};
-		NQueens object = new NQueens(data,row,dimention);
+	public void testGetNQueen() throws IOException {
+		NQueens object = new NQueens(board,row,dimension);
 		int actual[][]= object.getNQueen();
 		assertArrayEquals(expected,actual);
 	}
--- SCF-Session5-Question3/src/meta/utility/NQueens.java
+++ SCF-Session5-Question3/src/meta/utility/NQueens.java
@@ -1,64 +1,113 @@
+/*
+ * This is Utility class for NQueens Problems Solution
+ * @author ABHISHEK BISHNOI
+ * @version 1.0
+ * @since 19-July-2019
+ */
 package meta.utility;
 
 public class NQueens {
-	int dimention;
+	int dimension;
 	int row;
 	int board[][];
 	int emptyBoard[][];
-	public NQueens( int board[][], int row, int dimention){
+	/*
+	 * it is Constructor For initialize variable 
+	 */
+	public NQueens( int board[][], int row, int dimension){
 		this.board = board;
 		emptyBoard = board;
 		this.row = row;
-		this.dimention = dimention;
+		this.dimension = dimension;
 	}
+	/*
+	 * this return array value of placed queens in board 
+	 * otherwise it returns empty board
+	 */
 	public int[][] getNQueen(){
-		boolean result = findSolution(board,row,dimention);
+		boolean result = findSolution(board,row,dimension);
 		if(result == true){
 			return board;
 		}
 		return emptyBoard;
 	}
 	/*
-	 * 
+	 * this Function Returns True if A solutions is possible for that's row in board
+	 * @param int[][] this contains current position of board 
+	 * @param int this contains current row index of board on which operation is performing
+	 * @param int this is the size of dimension
+	 * @return boolean value for possible solution
 	 */
-	private boolean findSolution(int board[][], int row, int dimention) {
+	private boolean findSolution(int board[][], int row, int dimension) {
 		boolean flag = false;
-		if(row>=dimention){
+		if(row>=dimension){
 			flag = true;
 			return flag;
 		}
-		for(int i=0; i<dimention; i++){
-			boolean result = placeQueenOrNot(board, row, i, dimention);
+		/*
+		 * logic for backtracking in Queen's Problem
+		 */
+		for(int i=0; i<dimension; i++){
+			boolean result = placeQueenOrNot(board, row, i, dimension);
+			// checking if placing a queen is possible or not
 			if(result==true){
-				board[row][i]=1;
-				boolean isSolve = findSolution(board,row+1,dimention);
+				board[row][i]=1; // updating value as 1
+				boolean isSolve = findSolution(board,row+1,dimension);
+				// calling recursively for next row value
 				if(isSolve==true){
-					return true;
+					flag = true;
+					return flag; // if possible then true
 				}
-				board[row][i]=0;
+				board[row][i]=0; // updating value to zero
 			}
 		}
-		return false;
+		return flag;
 	}
 	/*
-	 * 
+	 * This Function Returns if The Queen can be placed or not without interrupting any other queen
+	 * @param int[][] this contains current position of board 
+	 * @param int this contains current row index of board on which operation is performing
+	 * @param int this contains current column index of board on which operation is performing
+	 * @param int this is the size of dimension
+	 * @return boolean value for possible solution
 	 */
 	private boolean placeQueenOrNot(int[][] board, int row, int column, int dimention) {
+		boolean flag = true;
+		// checking in row values
 		for(int i=0; i<row; i++){
 			if(board[i][column]==1){
-				return false;
+				flag =  false;
+				return flag;
 			}
 		}
+		// checking in column values
+		for(int i=0; i<column; i++){
+			if(board[row][i]==1){
+				flag =  false;
+				return flag;
+			}
+		}
+		// checking in top left diagonal
 		for(int i=row, j=column; i>=0 && j>= 0; i--, j--){
 			if(board[i][j]==1){
-				return false;
+				flag = false;
+				return flag;
 			}
 		}
+		// checking in bottom left diagonal
 		for(int i=row, j=column; j>0 && i<dimention;i++,j--){
 			if(board[i][j]==1){
-				return false;
+				flag =  false;
+				return flag;
 			}
 		}
-		return true;
+		// checking in top right diagonal
+		for(int i=row, j=column; i>0 && j<dimention;i--,j++){
+			if(board[i][j]==1){
+				flag = false;
+				return flag;
+			}
+		}
+		return flag;
 	}
 }
