a99f16ccd4c73fc121c5b88efbd3f472f9da143d
ABHISHEK BISHNOI <abhishek.bishnoi@metacube.com>
2019-07-28 15:06:05 +0530

    Question 1 Of Session 6 After Changes

--- SCF-Session6-Question1/src/meta/test/IntSetTest.java
+++ SCF-Session6-Question1/src/meta/test/IntSetTest.java
@@ -38,7 +38,7 @@ public class IntSetTest {
 	j++;
 	}
 	}
-	assertArrayEquals(expectedArray, intSet.getIntArray((intSet.getCompliment(intSet).getIntSet())));
+	assertArrayEquals(expectedArray, intSet.getCompliment(intSet).getIntSet());
 	}
 
 	/**
@@ -50,7 +50,7 @@ public class IntSetTest {
 	int input2[]=new int[] { 3, 2, 5, 4, 7, 8 };
 	IntSet obj1 = new IntSet(input1);
 	IntSet obj2 = new IntSet(input2);
-	assertArrayEquals(new int[] { 1, 3, 5, 8, 12, 15, 18, 40, 2, 4, 7 }, obj1.getIntArray(obj1.union(obj1, obj2).getIntSet()));
+	assertArrayEquals(new int[] { 1, 3, 5, 8, 12, 15, 18, 40, 2, 4, 7 }, obj1.union(obj1, obj2).getIntSet());
 
 	}
 
--- SCF-Session6-Question1/src/meta/utility/IntSet.java
+++ SCF-Session6-Question1/src/meta/utility/IntSet.java
@@ -8,27 +8,49 @@
  */
 package meta.utility;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class IntSet implements IntSetInterface{
-	private final Set<Integer> set = new LinkedHashSet<Integer>();
+	private final int[] setArray;
 	/*
 	 * IntSet save as a Set value
 	 * @param int[] array as a set value
 	 */
-	public IntSet(int array[]){
-		for( int value : array ){
-			set.add( value );
+	public IntSet(int inputArray[]){
+		setArray = getSetArray(inputArray);
+	}
+	/**
+	 * 
+	 * @param inputArray input Array
+	 * @return return Set data type Array
+	 * Complexity O(n^2)
+	 */
+	private int[] getSetArray(int[] inputArray) {
+		List<Integer> listSet = new ArrayList<Integer>();
+		for(int i=0;i<inputArray.length;i++){
+            boolean isDistinct = false;
+            for(int j=0;j<i;j++){
+                if(inputArray[i] == inputArray[j]){
+                    isDistinct = true;
+                    break;
+                }
+            }
+            if(!isDistinct){
+            	listSet.add(inputArray[i]);
+            }
 		}
+		int setArray[] = listSet.stream().mapToInt(i -> i).toArray();
+		return setArray;
 	}
 	/*
 	 * @return Set<Integer> getter method
 	 */
-	public Set<Integer> getIntSet(){
-		return this.set;
+	public int[] getIntSet(){
+		return setArray;
 	}
 	/*
 	 * @return Set<Integer> return set of range 1-1000
@@ -49,9 +71,11 @@ public class IntSet implements IntSetInterface{
 	@Override
 	public boolean isMember( int x ) {
 		boolean result = false;
-		if( set.contains(x) ){
-			result = true;
-			return result;
+		for(int setValue: setArray){
+			if(setValue == x){
+				result=true;
+				break;
+			}
 		}
 		return result;
 	}
@@ -64,13 +88,7 @@ public class IntSet implements IntSetInterface{
 	@Override
 	public int size() {
 		int size = 0;
-		/*
-		 * for(int x: set){
-		 * size++ ;
-		 * }
-		 * this for loop contains O(n) Complexity to calculate size 
-		 */
-		size = set.size(); 
+		size = setArray.length; 
 		// Contains O(1) Complexity to calculate size
 		return size;
 	}
@@ -84,12 +102,14 @@ public class IntSet implements IntSetInterface{
 	@Override
 	public boolean isSubSet( IntSet subSet ) {
 		boolean result = true;
-		final Set< Integer > currentSet = subSet.getIntSet();
+		final int[] currentSet = subSet.getIntSet();
 		//
-		for(int value : currentSet ){
-			if( !( set.contains(value) ) ){
-				result = false;
-				return false;
+		for(int currentSetvalue : currentSet ){
+			for(int setValue : setArray){
+				if( setValue != currentSetvalue ){
+					result = false;
+					return result;
+				}
 			}
 		}
 		return result;
@@ -103,29 +123,20 @@ public class IntSet implements IntSetInterface{
 	 */
 	@Override
 	public IntSet union(IntSet set1, IntSet set2) {
-		final Set<Integer> currentSet1 = set1.getIntSet();
-		final Set<Integer> currentSet2 = set2.getIntSet();
-		final Set<Integer> unionSet = new LinkedHashSet<Integer>();
-		unionSet.addAll(currentSet1);
-		unionSet.addAll(currentSet2);
-		int unionArray [] = getIntArray(unionSet);
+		final int[] currentSet1 = set1.getIntSet();
+		final int[] currentSet2 = set2.getIntSet();
+		final List<Integer> union = new LinkedList<Integer>();
+		for(int i=0; i<currentSet1.length; i++){
+			union.add(currentSet1[i]);
+		}
+		for(int i=0; i<currentSet2.length; i++){
+			union.add(currentSet2[i]);
+		}
+		int unionArray [] = union.stream().mapToInt(i -> i).toArray();
 		IntSet returnObject = new IntSet(unionArray);
 		return returnObject;
 	}
-	/*
-	 * @param Set<integer> value in Set
-	 * @return int[] an array value
-	 */
-	public int[] getIntArray(Set<Integer> unionSet) {
-		int resultArray[] = new int[unionSet.size()];
-		int index = 0;
-		for( int value : unionSet){
-			resultArray[index] = value;
-			index++;
-		}
-
-		return resultArray;
-	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see meta.utility.IntSetInterface#getCompliment(meta.utility.IntSet)
@@ -135,12 +146,20 @@ public class IntSet implements IntSetInterface{
 	@Override
 	public IntSet getCompliment(IntSet set) {
 		IntSet universal = getUniversalSet();
-		Set<Integer> universalSet = universal.getIntSet();
-		Set<Integer> currentSet = set.getIntSet();
+		int[] universalSet = universal.getIntSet();
+		int[] currentSet = set.getIntSet();
 		List<Integer> list = new LinkedList<Integer>();
-		for(int value : universalSet){
-			if(!currentSet.contains(value)){
-				list.add(value);
+		for(int universalSetValue : universalSet){
+			boolean isCommon=false;
+			for(int currentSetValue: currentSet){
+				if(currentSetValue==universalSetValue){
+					isCommon = true;
+					break;
+				}
+			}
+			if(!isCommon){
+				list.add(universalSetValue);
+				break;
 			}
 		}
 		int[] array = list.stream().mapToInt(i -> i).toArray();
