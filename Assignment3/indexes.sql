76585b1c23876e0e98eaf0e0635664e176ce60ce
ABHISHEK BISHNOI <abhishek.bishnoi@metacube.com>
2019-08-21 14:00:10 +0530

    With Changes in Assignment 3

--- Assignment3/indexes.sql
+++ Assignment3/indexes.sql
@@ -1,19 +1,19 @@
 /**
 * creating index for product
 */
-create index product_index on product(product_id);
+create index product_index on product(product_name);
 SHOW INDEXES FROM product;
  
 /**
-* creting index cluster for order 
+* creting indexes 
 */
-create index order_index on orders(customer_id);
+create index order_index_user on orders(customer_id);
+create index order_index_time on orders(ordertime);
 show indexes from orders;
 
 /**
-* creating cluster on parent key
+* creating indexes
 */
-CREATE INDEX parent_index ON category(parent_key);
-
+CREATE INDEX category_index ON category(parent_key);
+CREATE INDEX category_index_name ON category(category_name);
 SHOW INDEXES FROM category;
-
